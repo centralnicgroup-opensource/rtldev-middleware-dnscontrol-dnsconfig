657f9c7897c7367bef6b7798c7a0379fbd4065f5
Kai Schwarz <kschwarz@hexonet.net>
2018-08-08 09:37:35 +0200

    added global TTL defaults

--- dnsconfig.js
+++ dnsconfig.js
@@ -6,14 +6,19 @@
 var REG_HX = NewRegistrar('hexonet', 'HEXONET');
 var DNS_HX = NewDnsProvider('hexonet', 'HEXONET');
 
-DEFAULTS(DefaultTTL(3600));
+// Set Default TTL for all RR - does not apply to NAMESERVER
+// https://github.com/StackExchange/dnscontrol/issues/99
+DEFAULTS(
+    {"ns_ttl":"3600"},
+    DefaultTTL(3600)
+);
 
 // Domains:
 D('abhoster.com', REG_HX, DnsProvider(DNS_HX),
-    NAMESERVER('ns1.ispapi.net'),
-    NAMESERVER('ns2.ispapi.net'),
-    NAMESERVER('ns3.ispapi.net'),
-    NAMESERVER('ns4.ispapi.net'),
+    NAMESERVER('ns1.ispapi.net.'),
+    NAMESERVER('ns2.ispapi.net.'),
+    NAMESERVER('ns3.ispapi.net.'),
+    NAMESERVER('ns4.ispapi.net.'),
     A('elk1', '10.190.234.178'),
     A('test', '56.123.54.12')
 );
