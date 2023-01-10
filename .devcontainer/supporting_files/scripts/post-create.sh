4cc0f4d4e5962059069934469cfe6752edbb7694
Asif Nawaz <asif.nawaz@centralnic.com>
2023-01-10 10:41:05 +0000

    build(docker): gitconfig and post-create updated

--- .devcontainer/supporting_files/scripts/post-create.sh
+++ .devcontainer/supporting_files/scripts/post-create.sh
@@ -1,3 +1,4 @@
 # generate go binary && create symlink
 cd rtldev-middleware-dnscontrol
 go install && sudo ln -sF ~/go/bin/dnscontrol /usr/local/bin/
+cd /usr/share/rtldev-middleware-dnscontrol-dnsconfig
