b54cd3b4c073eec7ebab8f3129c837df40b7ae6c
Asif Nawaz <asif.nawaz@centralnic.com>
2023-01-10 13:15:11 +0000

    build(docker): configuration changes and post-create tweaks

--- .devcontainer/supporting_files/scripts/post-create.sh
+++ .devcontainer/supporting_files/scripts/post-create.sh
@@ -1,4 +1,16 @@
-# generate go binary && create symlink
+#!/bin/bash
+# NOTE: This file will be executed as remoteUser (devcontainer.json)
+echo "=> Script: post-create.sh Executed by: $(whoami)"
+# Install Z-shell
+source <(curl -sL git.io/zi-loader); zzinit
+
+sudo npm install --silent --progress=false --global commitizen@latest cz-conventional-changelog@latest
+
+# shellcheck source=/dev/null
+source ~/.zshrc
+
+# generate go binary && create a bin symlink
 cd rtldev-middleware-dnscontrol
+git checkout master && git fetch origin -p --tags && git pull
 go install && sudo ln -sF ~/go/bin/dnscontrol /usr/local/bin/
 cd /usr/share/rtldev-middleware-dnscontrol-dnsconfig
