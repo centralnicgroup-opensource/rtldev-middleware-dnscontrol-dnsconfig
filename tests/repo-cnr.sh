3a27c1e041926275972c130c70ff95687225af59
Kai Schwarz <kai.schwarz@centralnic.com>
2025-01-17 14:50:29 +0100

    build(tests): updated test script

--- tests/repo-cnr.sh
+++ tests/repo-cnr.sh
@@ -5,13 +5,11 @@ export CNR_UID=$CNR_TEST_USER
 export CNR_PW=$CNR_TEST_PASSWORD
 export CNR_DOMAIN="dnscontroltest2-cnr.com"
 # echo =================== NO DEBUG
+# unset CNR_DEBUGMODE
+# cd /usr/share/rtldev-middleware-dnscontrol-dnsconfig/rtldev-middleware-dnscontrol/integrationTest
+# go test -v -verbose -provider CNR 
 
-unset CNR_DEBUGMODE
-cd /usr/share/rtldev-middleware-dnscontrol-dnsconfig/rtldev-middleware-dnscontrol/integrationTest
-go test -v -verbose -provider CNR 
-
-echo =================== DEBUG
-
-export CNR_DEBUGMODE=1
+#echo =================== DEBUG
+export CNR_DEBUGMODE=2
 cd /usr/share/rtldev-middleware-dnscontrol-dnsconfig/rtldev-middleware-dnscontrol/integrationTest
 go test -v -verbose -provider CNR
